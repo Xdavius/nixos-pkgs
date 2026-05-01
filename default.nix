a87f83a9cd76c10cd005962865b7875a9e2480a4
Davius <66792063+Xdavius@users.noreply.github.com>
2026-05-01 23:08:58 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 
-{
-  tachyfy = pkgs.callPackage ./pkgs/tachyfy {};
-}
+let
+  dir = ./pkgs;
+
+  names = builtins.attrNames (builtins.readDir dir);
+
+  isDir = name: (builtins.readDir dir)."${name}" == "directory";
+
+  pkgsList = builtins.filter isDir names;
+in
+builtins.listToAttrs (map (name: {
+  name = name;
+  value = pkgs.callPackage (dir + "/${name}") {};
+}) pkgsList)
